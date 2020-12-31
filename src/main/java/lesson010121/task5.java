0d142abe87a61108fda9a5e71925113a987a13e6
Vats Profil <vats.profil@gmail.com>
2020-12-31 15:03:35 +0400

    commit

---
+++ src/main/java/lesson010121/task5.java
@@ -0,0 +1,52 @@
+package lesson010121;
+
+import java.util.Optional;
+
+public class task5 {
+
+    public static void main(String[] args) {
+
+        findDivisorsSum(100);
+
+
+
+    }
+
+
+    public static void findDivisorsSum(int number){
+        int sum=0;
+        for(int i=1;i<number;i++){
+
+            for(int j=1;j<i;j++){
+
+                if(i%j==0) {
+                    sum = sum + j;
+                }
+
+
+            }if(i<sum){
+                System.out.println(i + " abundant number");
+                sum=0;
+            }
+            else if(i>sum){
+                System.out.println(i + " deficient number");
+                sum=0;
+            }
+
+            else if(i==sum){
+                System.out.println(i + " perfect number");
+                sum=0;
+            }
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+}
